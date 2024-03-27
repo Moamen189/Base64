5fe942606e029237e8fc69ac0f31bbb5e7b8a50b
Moamen ASHRAF <moamen.ashraf@intalio.com>
2024-03-27 11:30:42 +0200

    Add project files.

---
+++ Base64/Controllers/PhotoController.cs
@@ -0,0 +1,44 @@
+﻿using Base64.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.IO;
+using System.Web;
+namespace Base64.Controllers
+{
+    public class PhotoController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult ConvertToBase64(IFormFile file)
+        {
+            if (file != null && file.Length > 0)
+            {
+                try
+                {
+                    using (var memoryStream = new MemoryStream())
+                    {
+                        file.CopyTo(memoryStream);
+                        byte[] imageData = memoryStream.ToArray();
+                        string base64String = Convert.ToBase64String(imageData);
+                        var model = new PhotoModel { Base64String = base64String };
+                        return PartialView("_Base64Result", model);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    ViewBag.ErrorMessage = $"Error occurred: {ex.Message}";
+                }
+            }
+            else
+            {
+                ViewBag.ErrorMessage = "Please select a file.";
+            }
+
+            return PartialView("_Error");
+        }
+    }
+}
---
+++ Base64/Models/PhotoModel.cs
@@ -0,0 +1,7 @@
+﻿namespace Base64.Models
+{
+    public class PhotoModel
+    {
+        public string Base64String { get; set; }
+    }
+}
